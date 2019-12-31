156b41eabb0269e6a3d5d69ceadb038bf9b5556c
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:44:15 -0600

    Rewriting as Laminas Project package

--- test/Metadata/ExceptionTest.php
+++ test/Metadata/ExceptionTest.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-hal for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-hal/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-hal for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-hal/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-hal/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace ZendTest\Expressive\Hal\Metadata;
+namespace MezzioTest\Hal\Metadata;
 
 use Generator;
+use Mezzio\Hal\Exception\ExceptionInterface as HalExceptionInterface;
+use Mezzio\Hal\Metadata\Exception\ExceptionInterface;
 use PHPUnit\Framework\TestCase;
-use Zend\Expressive\Hal\Exception\ExceptionInterface as HalExceptionInterface;
-use Zend\Expressive\Hal\Metadata\Exception\ExceptionInterface;
 
 class ExceptionTest extends TestCase
 {
