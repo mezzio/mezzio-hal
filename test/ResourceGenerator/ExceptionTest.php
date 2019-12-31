f3313a4edb06690134ecb8ac33f4f803416d1025
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:44:32 -0600

    Rewriting as Laminas Project package

--- test/ResourceGenerator/ExceptionTest.php
+++ test/ResourceGenerator/ExceptionTest.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-hal for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-hal/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-hal for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-hal/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-hal/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace ZendTest\Expressive\Hal\ResourceGenerator;
+namespace MezzioTest\Hal\ResourceGenerator;
 
 use Generator;
+use Mezzio\Hal\Exception\ExceptionInterface as HalExceptionInterface;
+use Mezzio\Hal\ResourceGenerator\Exception\ExceptionInterface;
 use PHPUnit\Framework\TestCase;
-use Zend\Expressive\Hal\Exception\ExceptionInterface as HalExceptionInterface;
-use Zend\Expressive\Hal\ResourceGenerator\Exception\ExceptionInterface;
 
 use function basename;
 use function glob;
